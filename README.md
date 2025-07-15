c89a95c4b42a6dd0c94f79dc7487848137b69ce8
Patrick ILUNGA <patriqyoo@gmail.com>
2025-07-15 08:13:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# SEAM Project - Reservoir Characterization
+
+> Analyse exploratoire des données sismiques du projet SEAM (SEG Wiki) et modélisation des caractéristiques du réservoir pétrolier.
+
+---
+
+## Objectif
+
+Ce projet vise à analyser les données sismiques du projet SEAM Phase I (lien web) afin d'explorer les caractéristiques du sous-sol et de modéliser les propriétés du réservoir pétrolier. 
+L’objectif est de fournir une analyse exploratoire des données (AED), extraire des attributs sismiques pertinents, et développer des modèles prédictifs (ML/Inversion) pour la caractérisation du réservoir.
+
+---
+
+## Structure du projet
+
+
+## Dépendances
+
+- Python 3.x
+- Bibliothèques : `segyio`, `numpy`, `pandas`, `matplotlib`, `scikit-learn`, `jupyter`, `streamlit`
---
+++ notebooks/load_seismic_trace.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 11 14:16:39 2025
+
+@author: patrickilunga
+"""
+
+import os
+import segyio
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Chemin vers ton fichier SEGY (on commence par le plus petit)
+filename = "data/raw/SEAM_Interpretation_Challenge_1_2DSparseGathers_Depth.sgy"
+
+
+# Ouvre le fichier en mode lecture
+with segyio.open(filename, "r", ignore_geometry=True) as f:
+    # Lis la première trace (index 0)
+    trace = f.trace[0]
+
+    # Affiche quelques informations
+    print(f"Nombre d'échantillons dans la trace : {len(trace)}")
+    print(f"Quelques premières valeurs : {trace[:10]}")
+
+    # Affiche la trace avec matplotlib
+    plt.figure(figsize=(10, 4))
+    plt.plot(trace)
+    plt.title("Première trace sismique (SEGY)")
+    plt.xlabel("Échantillon")
+    plt.ylabel("Amplitude")
+    plt.grid(True)
+    plt.tight_layout()
+    # Sauvegarde la figure
+    plt.savefig("results/example_seismic_trace.png")  
+    plt.show()
